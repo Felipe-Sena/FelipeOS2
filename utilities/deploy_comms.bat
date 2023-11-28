f4ac4c5ac3532fbea9a9394bc058a36edab7dfc7
Felipe-Sena <lipesenacosta@gmail.com>
2023-11-27 21:40:03 -0300

    Added a .bat tool to run the command deployer tool

---
+++ utilities/deploy_comms.bat
@@ -0,0 +1,25 @@
+@echo off
+
+:start
+cls
+echo Do you want to compile global commands or just local commands? (1/2)
+set /p choice=
+if %choice% == 1 (
+    goto global
+) else (
+    if %choice% == 2 (
+        goto local
+    ) else (
+        goto start
+    )
+)
+:global
+cd ./../js/sub_programs
+node ./deploy_commands_global.js
+goto :eof
+
+
+:local
+cd ./../js/sub_programs
+node ./deploy_commands_local.js
+goto :eof
