4b34fc4321750246d60c491426269b0f2d571ee0
Felipe-Sena <lipesenacosta@gmail.com>
2023-12-04 17:25:41 -0300

    Implemented the count program

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ REQUIRES
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+const data = require('./json/data.json');
+const count = require('./js/commands/chat/count');
+const strings = require('./json/strings.json');
 const core = require('./js/sub_programs/core');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
@@ -54,6 +57,7 @@ const client = new Client({ intents: [
 
 client.commands = new Collection();
 client.cooldowns = new Collection();
+
 
 /*
 ----------------------------
@@ -140,6 +144,55 @@ ENTRY POINT
     // TODO: Splice the messages to organize them into arrays
     client.on(Events.MessageCreate, (message_event) => {
         console.log(message_event.content);
+
+        // Variable definitons
+
+        const random_number = Math.random();
+
+        // #region Count
+
+        if (message_event.channelId === data.channel_ids.count && /^[0-9]+$/.test(message_event.content) === true) {
+            console.log(message_event.author.id);
+            count.count_function(message_event.author.id, message_event.content);
+
+            // Cleanup below
+
+            count.count_output_event.removeAllListeners('success');
+            count.count_output_event.removeAllListeners('fail');
+            count.count_output_event.removeAllListeners('success_special_69');
+            count.count_output_event.removeAllListeners('success_special_100');
+            count.count_output_event.removeAllListeners('critical_error');
+
+            count.count_output_event.on('success', () => {
+                message_event.react('✅');
+            });
+
+            count.count_output_event.on('success_special_69', () => {
+                message_event.react('6️⃣')
+                    .then(message_event.react('9️⃣'))
+                    .then(message_event.react('✅'));
+            });
+
+            count.count_output_event.on('success_special_100', () => {
+                message_event.react('💯')
+                    .then(message_event.react('✅'));
+            });
+
+            count.count_output_event.on('fail', () => {
+                console.log('fail');
+                message_event.react('❌');
+                message_event.reply(strings.count_fail[Math.floor((random_number * strings.count_fail.length))]);
+            });
+
+            count.count_output_event.on('critical_error', () => {
+                console.log(`${bad('[ERROR]')} ${warn('The count.json file could not be read due to possible corruption.')}`);
+                message_event.react('😥');
+                message_event.reply('A critical error ocurred.');
+            });
+
+        }
+
+        // #endregion
     });
 
     /*
@@ -150,6 +203,9 @@ ENTRY POINT
 
     core.console_output_event.on('output', (output) => {
         switch (output) {
+            case 'help':
+                console.log('hello this is help');
+                break;
             case 'quit': process.exit(0);
         }
     });
